4899ebc9584862769e1439de2f3c418ed2d0cc21
alancom1l <alan.doarist@gmail.com>
2025-03-14 18:50:35 -0400

    Ajustando posicao inicial do mapa manualmente

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -3,7 +3,7 @@ import MarkerList from './MarkerList';
 
 const MapView: React.FC = () => {
   return (
-    <MapContainer center={[-14.1306, -47.5205]} zoom={10} style={{ height: '100vh', width: '100%' }}>
+    <MapContainer center={[-14.0100, -47.5205]} zoom={10} style={{ height: '100vh', width: '100%' }}>
       <TileLayer 
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
